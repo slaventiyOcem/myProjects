cd12e37d93abe457b177d66faa90765a7a50fc6e
Вячеслав <119728048+slaventiyOcem@users.noreply.github.com>
2023-09-01 23:44:38 +0300

    Add files via upload

---
+++ home/js/rating.js
@@ -0,0 +1,29 @@
+"use strict"
+const ratings = document.querySelectorAll('.rating');
+
+if (ratings.length > 0) {
+    initRatings();
+}
+//Основная функция
+function initRatings(){
+    let ratingActive, ratingValue;
+    //перебор рейтинга
+    for(let index = 0; index < ratings.length; index++) {
+        const rating = ratings[index];
+        initRating(rating);
+    }
+
+    function initRating(rating){
+        initRatingVars(rating);
+
+        setRatingActiveWidth();
+    }
+    function initRatingVars(rating) {
+        ratingActive = rating.querySelector('.rating__active');
+        ratingValue = rating.querySelector('.rating__value');
+    }
+    function setRatingActiveWidth(index = ratingValue.innerHTML){
+        const ratingActiveWidth = index / 0.05;
+        ratingActive.style.width = `${ratingActiveWidth}%`;
+    }
+}
